8d7443a44f39c970596deb53467a53249fae59a5
Kap <kacperlm@gmail.com>
2021-10-05 17:38:36 +0200

    first commit

---
+++ dndapp/urls.py
@@ -0,0 +1,35 @@
+"""dndapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+# URLs, zupełnie nic ciekawego
+
+from django.contrib import admin
+from django.urls import path
+from charsheets import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # auth
+    path('signup/', views.signupuser, name='signupuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
+    path('login/', views.loginuser, name='loginuser'),
+
+    # charsheets
+    path('', views.home, name='home'),
+    path('menu', views.menu, name='menu'),
+    path('create/', views.createchar, name='createchar'),
+    path('character/<int:character_pk>', views.viewcharacter, name='viewcharacter'),
+]
